adfa43565d931035e17fe032f521d13e55a0954b
Kamel Houmouad <kahoumou@student.42w>
2024-12-02 14:32:09 +0100

    .

--- test.c
+++ test.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
-
+int f_test(){
+   return -100; 
+}
 
 
 
 int main()
 {
-    int info = 0;
+    int info = f_test();
     printf("hello conflict %d ...\n", info);
     return(0);
 }
